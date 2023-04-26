0f49e483e4ecd5c514968efb0a05f7b301c859e1
fivenames <liang0354@gmail.com>
2023-04-26 13:47:13 +0800

    Added all Project files

--- ai/2-uncertainty/probability.py
+++ ai/2-uncertainty/probability.py
@@ -4,6 +4,9 @@ P(A|B) = P(B|A) * ( P(A) / P(B) ); This can be used for eg. 80% of counterfeit b
 
 Recall H2 Math probability Venn diagram: Negation( P(A) = 1 - P(¬A) ), Inclusion-Exclusion( P(A V B) = P(A) + P(B) - P(A ∧ B) ), and;
 Marginalisation( P(A) = P(A ∧ B) + P(A ∧ ¬B) ) == Conditioning( P(A) = P(A|B)P(B) + P(A|¬B)P(¬B) )
+
+Bayesian Network - data structure that represents the dependencies among random variable, it is implemented using a directed graph with each node representing a random variable
+and has the probability distribution: P(X|parents(X)); To calculate the joint probability, P(grandparents ∧ parents ∧ X) = P(grandparent)P(parent|grandparent)P(X|parent)
 '''
 
 #
