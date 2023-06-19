2ca73795d9b76a112b68484607a90a5616f30daf
fivenames <liang0354@gmail.com>
2023-06-19 23:18:32 +0800

    minor change to learning notes

---
+++ ai/5-neuralnetworks/networks.py
@@ -0,0 +1,6 @@
+'''
+Artificial Neural Networks models a mathematical function from inputs to outputs based on the structure and parameters of the network.
+It also allows for learning the network's parameters based on data.
+For eg. the activation function mentioned in machine learning: h(x) = w0 + w1x1 + w2x2 is a simple neural network. It can be expressed in the form of 2 nodes connecting to 1 node,
+for which the 2 nodes are input data - x1 and x2, and the other node is the output, w0 + w1x1 + w2x2.
+'''
