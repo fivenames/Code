3510ff5aa187eb2d0d6b605c1df34522cb735e0d
fivenames <liang0354@gmail.com>
2024-11-12 10:57:33 +0800

    completed assignment 5

--- CS2040C/5 - SSSP/shortest_path_test.cpp
+++ CS2040C/5 - SSSP/shortest_path_test.cpp
@@ -5,50 +5,39 @@
 using namespace std;
 
 int main(){
-  Graph g0(4);
-  g0.addEdge(0, 1, 1);
-  g0.addEdge(0, 2, 2);
-  g0.addEdge(1, 3, 5);
-  g0.addEdge(2, 3, 1);
+  Graph g(25);
+  g.addEdge(0, 1, 82);
+  g.addEdge(0, 2, 72);
+  g.addEdge(1, 3, 145);
+  g.addEdge(1, 5, 119);
+  g.addEdge(1, 6, 152);
+  g.addEdge(2, 1, 38);
+  g.addEdge(2, 4, 59);
+  g.addEdge(3, 1, 30);
+  g.addEdge(3, 7, 168);
+  g.addEdge(4, 8, 174);
+  g.addEdge(5, 6, 38);
+  g.addEdge(6, 3, 186);
+  g.addEdge(6, 4, 139);
+  g.addEdge(6, 5, 99);
+  g.addEdge(6, 7, 107);
+  g.addEdge(7, 4, 82);
+  g.addEdge(7, 8, 133);
+  g.addEdge(7, 9, 166);
+  g.addEdge(8, 2, 112);
+  g.addEdge(8, 3, 160);
+  g.addEdge(8, 6, 50);
+  g.addEdge(8, 7, 180);
+  g.addEdge(9, 0, 98);
+  g.addEdge(9, 3, 52);
+  g.addEdge(9, 5, 70);
 
-  for (int d = 0; d < 4; d++) {
-    Path p = shortestPath(g0, 0,d);
-    std::cout << "The shortest distance from " << 0 << " to " << d << " is " << p.total_distance() << " and the path is: { ";
-    for (int i = 0; i < p.path().size(); i++) {
-      std::cout << p.path()[i];
-      if (i!=p.path().size()-1) std::cout << " , ";
-    }
-    std::cout << " }" << endl;
+  Path p = shortestPath(g, 6, 1);
+  std::cout << "The shortest distance from " << 6 << " to " << 1 << " is " << p.total_distance() << " and the path is: { ";
+  for (int i = 0; i < p.path().size(); i++) {
+    std::cout << p.path()[i];
+    if (i!=p.path().size()-1) std::cout << " , ";
   }
-  
-  std::cout << "Next Graph" << endl;
-  Graph g(8);
-  g.addEdge(0, 1, 5);
-  g.addEdge(0, 7, 8);
-  g.addEdge(0, 4, 9);
-  g.addEdge(1, 3, 15);
-  g.addEdge(1, 2, 12);
-  g.addEdge(1, 7, 4);
-  g.addEdge(3, 6, 9);
-  g.addEdge(2, 3, 3);
-  g.addEdge(2, 6, 11);
-  g.addEdge(4, 7, 5);
-  g.addEdge(4, 5, 4);
-  g.addEdge(4, 6, 20);
-  g.addEdge(5, 2, 1);
-  g.addEdge(5, 6, 13);
-  g.addEdge(7, 5, 6);
-  g.addEdge(7, 2, 7);
-
-  for (int d = 0; d < 8; d++) {
-    Path p = shortestPath(g, 0,d);
-    std::cout << "The shortest distance from " << 0 << " to " << d << " is " << p.total_distance() << " and the path is: { ";
-    for (int i = 0; i < p.path().size(); i++) {
-      std::cout << p.path()[i];
-      if (i!=p.path().size()-1) std::cout << " , ";
-    }
-    std::cout << " }" << endl;
-  }
-
+  std::cout << " }" << endl;
 }
 
